1467d37e8b0460824f7663d94d199213db766b38
shaonxxx <763119781@qq.com>
2019-08-20 20:43:12 +0800

    添加配置信息

--- src/main/java/com/woniu/woniuticket/cinema/controller/FilmController.java
+++ src/main/java/com/woniu/woniuticket/cinema/controller/FilmController.java
@@ -3,5 +3,6 @@ package com.woniu.woniuticket.cinema.controller;
 public class FilmController {
     public void test(){
         System.out.println("123");
+        System.out.println("123");
     }
 }
