598ae3aef098c18f3b3f983b49b8c25aebd3c8d9
ljt <ljt@163.com>
2019-08-22 12:04:04 +0800

    影片批量删除

--- src/main/java/com/woniu/woniuticket/cinema/controller/FilmController.java
+++ src/main/java/com/woniu/woniuticket/cinema/controller/FilmController.java
@@ -9,12 +9,10 @@ import com.woniu.woniuticket.cinema.service.FilmService;
 import com.woniu.woniuticket.cinema.utils.ImgUpload;
 import com.woniu.woniuticket.cinema.vo.FilmVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
