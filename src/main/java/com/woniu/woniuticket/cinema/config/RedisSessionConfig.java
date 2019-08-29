d72b265a18d10e89b20003c570a50b0a38b40a05
MrHuangYuLong <824510243@qq.com>
2019-08-29 12:07:42 +0800

    redis add

---
+++ src/main/java/com/woniu/woniuticket/cinema/config/RedisSessionConfig.java
@@ -0,0 +1,9 @@
+package com.woniu.woniuticket.cinema.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisSessionConfig {
+}
